0155137cc0ded894fc4e536340aedada43490028
Sax Dhiut <mail@iyxeyl.me>
2025-04-18 15:28:27 +0400

    feat: implement session-based redirection in main page for authentication flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
-	return (
-		<main>
-			<h1>Hello</h1>
-		</main>
-	)
+import { auth } from "@/lib/auth/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
+
+export default async function Index() {
+	const session = await auth.api.getSession({
+		headers: await headers()
+	})
+
+	if (!session?.session.userId) redirect("/auth")
+
+	redirect("/app")
 }
