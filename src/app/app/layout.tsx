23a45b91cdf95944a65d613d63e6537d1f98d4f9
Sax Dhiut <mail@iyxeyl.me>
2025-04-18 15:51:01 +0400

    refactor: adjust layout structure by moving SidebarTrigger above main content for improved styling and responsiveness

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -11,12 +11,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 		<SidebarProvider>
 			<AppSidebar />
 			<SidebarInset>
-				<main className={cn("p-4")}>
-					<div className={cn("fixed top-4 left-4 z-10")}>
-						<SidebarTrigger
-							className={cn("bg-sidebar", "cursor-pointer", "size-8")}
-						/>
-					</div>
+				<div className={cn("fixed top-4 left-4 z-10")}>
+					<SidebarTrigger
+						className={cn("bg-sidebar", "cursor-pointer", "size-8")}
+					/>
+				</div>
+				<main className={cn("p-4 container mx-auto max-w-3xl")}>
 					{children}
 				</main>
 			</SidebarInset>
